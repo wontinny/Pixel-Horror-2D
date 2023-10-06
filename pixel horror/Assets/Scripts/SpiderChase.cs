3110ca95f04b287df6006aceeb4d55768119c2a8
wontinny <brandon1won@gmail.com>
2023-10-05 23:53:55 -0500

    chases player when entering a certain distance

--- pixel horror/Assets/Scripts/SpiderChase.cs
+++ pixel horror/Assets/Scripts/SpiderChase.cs
@@ -4,7 +4,17 @@ public class SpiderChase : MonoBehaviour
 {
     public GameObject Player;
     public float speed = 1f;
-    public float distance;
+    private float chaseDistance = 1f;
+    private bool isChasing = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        {
+            // Spider has touched the player, so stop chasing.
+            isChasing = false;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +25,26 @@ public class SpiderChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, Player.transform.position) - 5;
-        Vector2 direction = Player.transform.position - transform.position;
+        float distance = Vector2.Distance(transform.position, Player.transform.position);
 
-        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed = Time.deltaTime);
+        if (distance <= chaseDistance)
+        {
+            // Spider is within chase range, so it should chase the player.
+            isChasing = true;
+        }
+        else
+        {
+            // Spider is outside of chase range, so it should stop chasing.
+            isChasing = false;
+        }
+
+        if (isChasing)
+        {
+            // Calculate the direction towards the player.
+            Vector2 direction = Player.transform.position - transform.position;
+
+            // Move the spider towards the player.
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
     }
 }
-
