8c3bf6357f35d8c9dc3a98568f7edb4997304ada
andy le <106003161+ande-le@users.noreply.github.com>
2023-10-15 14:27:26 -0500

    Animations and Transition Scenes
    added fade in/out for transition animations and able to transition scenes with spider test

--- pixel horror/Assets/Scripts/SceneController.cs
+++ pixel horror/Assets/Scripts/SceneController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
     public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
     private void Awake()
     {
         if (instance == null)
@@ -21,6 +22,14 @@ public class SceneController : MonoBehaviour
 
     public void NextLevel()
     {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
     }
 }
