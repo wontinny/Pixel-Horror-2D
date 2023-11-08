e3e81828ac64222690ddd1f88e1603bdb110a7ac
John Pham <phambinouh@gmail.com>
2023-11-08 01:37:38 -0600

    Sketch domain area

--- pixel horror/Assets/Scripts/DomainExpansion.cs
+++ pixel horror/Assets/Scripts/DomainExpansion.cs
@@ -8,6 +8,12 @@ public class DomainExpansion : MonoBehaviour
     public Enemy enemyScript;
     [SerializeField] private AudioSource domainSound;
     [SerializeField] private GameObject panCamera;
+    [SerializeField] private GameObject DomainA;
+    [SerializeField] private GameObject DomainB;
+    [SerializeField] private GameObject DomainC;
+    [SerializeField] private GameObject DomainD;
+    [SerializeField] private GameObject DomainE;
+    [SerializeField] private GameObject DomainF;
 
     bool DomainExpanded = false;
     private void HealthCheck()
@@ -16,17 +22,33 @@ public class DomainExpansion : MonoBehaviour
         {
             DomainExpanded = true;
             domainSound.Play();
-            gameObject.transform.position = new Vector2(0.15f, -0.25f); 
             StartCoroutine(TimeDelay());
         }
     }
 
     IEnumerator TimeDelay()
-    {
+    {        
+        enemyScript.enabled = false;
+        yield return new WaitForSeconds(1f); // Delay to let the Player's knockback affect Mary
+        gameObject.transform.position = new Vector2(0.15f, -0.25f); 
         panCamera.SetActive(true);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
+        DomainA.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DomainB.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DomainC.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DomainD.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DomainE.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DomainF.SetActive(true);
         panCamera.SetActive(false);
+        enemyScript.enabled = true;
+
     }
+
     public void Update()
     {
         HealthCheck();
