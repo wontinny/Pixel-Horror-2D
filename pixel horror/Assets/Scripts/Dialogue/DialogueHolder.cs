613421a350c9ebba4930bdb4aa4f5b052da60deb
John Pham <phambinouh@gmail.com>
2023-11-08 14:32:15 -0600

    Finished Mary dialogue

--- pixel horror/Assets/Scripts/Dialogue/DialogueHolder.cs
+++ pixel horror/Assets/Scripts/Dialogue/DialogueHolder.cs
@@ -6,8 +6,11 @@ namespace DialogueSystem
 {
     public class DialogueHolder : MonoBehaviour
     {
+        [SerializeField] private GameObject dialogBackground;
+
         private void Awake()
         {
+            dialogBackground.SetActive(true);
             StartCoroutine(dialogueSequence());
         }
 
@@ -19,6 +22,8 @@ namespace DialogueSystem
                 transform.GetChild(i).gameObject.SetActive(true);
                 yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
             }
+            gameObject.SetActive(false);
+            dialogBackground.SetActive(false);
         }
 
         private void Deactivate()
