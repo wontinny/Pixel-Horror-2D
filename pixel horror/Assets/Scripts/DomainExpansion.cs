ce55fff3c1a5c20c62c3fe6b3d94b26c149436d6
John Pham <phambinouh@gmail.com>
2023-11-07 22:21:23 -0600

    Working on domain expansion

--- pixel horror/Assets/Scripts/DomainExpansion.cs
+++ pixel horror/Assets/Scripts/DomainExpansion.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class DomainExpansion : MonoBehaviour
 {
     public Enemy enemyScript;
+    [SerializeField] private AudioSource domainSound;
 
+    bool DomainExpanded = false;
     private void HealthCheck()
     {
-        if (enemyScript.CurrentHealth < 50)
+        if (enemyScript.CurrentHealth <= 90 && DomainExpanded == false)
         {
-            Destroy(gameObject);
+            DomainExpanded = true;
+            domainSound.Play();
+            gameObject.transform.position = new Vector2(0.15f, -0.25f); 
+            Time.timeScale = 0f;
+            StartCoroutine(TimeFreeze());
+            Time.timeScale = 1f;
         }
+    }
+
+    IEnumerator TimeFreeze()
+    {
+        yield return new WaitForSeconds(2);
+    }
+    public void Update()
+    {
+        HealthCheck();
     }
 }
