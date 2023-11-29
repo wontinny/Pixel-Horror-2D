8e5c0b901cb55d219dc532730da99771bcb860f3
andy le <106003161+ande-le@users.noreply.github.com>
2023-11-28 18:18:27 -0600

    intro video
    got it working through our scenes

---
+++ pixel horror/Assets/Scripts/videoScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class videoScript : MonoBehaviour
+{
+
+    VideoPlayer Video;
+
+    void Awake()
+    {
+        Video = GetComponent<VideoPlayer>();
+        Video.Play();
+        Video.loopPointReached += CheckOver;
+
+
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(2);//the scene that you want to load after the video has ended.
+    }
+}
